922851f285894dae8a6544736bdcb480c6d5eca2
hkhb <h14hbk@gmail.com>
2025-03-06 11:39:27 +0900

    ダミーデータを変更

--- src/page.tsx
+++ src/page.tsx
@@ -13,14 +13,14 @@ interface TodoItem {
   updatedAt?: Date;   // 更新日時（任意）
 }
 const todoItems:TodoItem[] = [
-  {id : 1, title: "掃除", description: "キッチンの掃除", completed: false, createdAt: new Date(20250304)},
+  {id : 1, title: "掃除", description: "キッチンの掃除", completed: false, createdAt: new Date(20250304), updatedAt: new Date(20250304)},
   {id : 2, title: "洗濯", completed: true, createdAt: new Date(20250304)},
   {id : 3, title: "料理", description: "味噌汁", completed: false, createdAt: new Date(20250304)},
-  {id : 4, title: "掃除", description: "トイレの掃除", completed: false, createdAt: new Date(20250304)},
-  {id : 5, title: "掃除", completed: false, createdAt: new Date(20250304)},
-  {id : 6, title: "掃除", description: "キッチンの掃除", completed: true, createdAt: new Date(20250304)},
-  {id : 7, title: "掃除", description: "キッチンの掃除", completed: false, createdAt: new Date(20250304)},
-  {id : 8, title: "掃除", description: "キッチンの掃除", completed: false, createdAt: new Date(20250304)},
+  {id : 4, title: "掃除", description: "1234", completed: false, createdAt: new Date()},
+  {id : 5, title: "掃除", completed: false, createdAt: new Date(), updatedAt: new Date()},
+  {id : 6, title: "掃除", description: "true", completed: true, createdAt: new Date(20250304)},
+  {id : 7, title: "1223", description: "null", completed: false, createdAt: new Date(20250304)},
+  {id : 8, title: "", description: "", completed: false, createdAt: new Date(20250304)},
   {id : 9, title: "掃除", description: "キッチンの掃除", completed: true, createdAt: new Date(20250304)},
   {id : 10, title: "掃除", description: "キッチンの掃除", completed: false, createdAt: new Date(20250304)},
 ]
