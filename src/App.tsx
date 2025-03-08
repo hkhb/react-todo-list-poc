939148136304ba703d28fedf430a6e305fcb65f4
hkhb <h14hbk@gmail.com>
2025-03-09 03:09:08 +0900

    モーダルコンポーネントを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import "./App.css"
+import Modal from './components/modal'
 import Lists from "./components/list.tsx"
 
 export interface TodoItem {
@@ -10,29 +12,55 @@ export interface TodoItem {
   updatedAt?: Date;   // 更新日時（任意）
 }
 
+const todoItems:TodoItem[] = [
+  {id : 1, title: "掃除", description: "キッチンの掃除", completed: false, createdAt: new Date(2025, 3, 4), updatedAt: new Date(2025, 3, 4)},
+  {id : 2, title: "洗濯", completed: true, createdAt: new Date(20250304)},
+  {id : 3, title: "料理", description: "味噌汁", completed: false, createdAt: new Date(2025, 3, 4)},
+  {id : 4, title: "掃除", description: "1234", completed: false, createdAt: new Date()},
+  {id : 5, title: "掃除", completed: false, createdAt: new Date(), updatedAt: new Date()},
+  {id : 6, title: "掃除", description: "true", completed: true, createdAt: new Date(2025, 3, 4)},
+  {id : 7, title: "1223", description: "null", completed: false, createdAt: new Date(2025, 3, 4)},
+  {id : 8, title: "", description: "", completed: false, createdAt: new Date(20250304)},
+  {id : 9, title: "掃除", description: "キッチンの掃除", completed: true, createdAt: new Date(2025, 3, 4)},
+  {id : 10, title: "掃除", description: "キッチンの掃除", completed: false, createdAt: new Date(20250304)},
+]
+
 function App() {
-  
-  const todoItems:TodoItem[] = [
-    {id : 1, title: "掃除", description: "キッチンの掃除", completed: false, createdAt: new Date(2025, 3, 4), updatedAt: new Date(2025, 3, 4)},
-    {id : 2, title: "洗濯", completed: true, createdAt: new Date(20250304)},
-    {id : 3, title: "料理", description: "味噌汁", completed: false, createdAt: new Date(2025, 3, 4)},
-    {id : 4, title: "掃除", description: "1234", completed: false, createdAt: new Date()},
-    {id : 5, title: "掃除", completed: false, createdAt: new Date(), updatedAt: new Date()},
-    {id : 6, title: "掃除", description: "true", completed: true, createdAt: new Date(2025, 3, 4)},
-    {id : 7, title: "1223", description: "null", completed: false, createdAt: new Date(2025, 3, 4)},
-    {id : 8, title: "", description: "", completed: false, createdAt: new Date(20250304)},
-    {id : 9, title: "掃除", description: "キッチンの掃除", completed: true, createdAt: new Date(2025, 3, 4)},
-    {id : 10, title: "掃除", description: "キッチンの掃除", completed: false, createdAt: new Date(20250304)},
-  ]
-  
+
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () =>{
+    setShowModal(true);
+  }
+
+  const closeModal = () =>{
+    setShowModal(false);
+  }
+  const HandleCancel = () => {
+    closeModal();
+  };
+  const HandleOk = () => {
+    closeModal();
+  }
+
   return (
-    <>
+    <div className='container'>
       <h1>TODOlist</h1>
-    <ul>
-    {todoItems.map((todoItem)=> (<Lists key={todoItem.id} {...todoItem}/> ))}
-    </ul>
-    </>
-  );
+      <div className='open-modal' >
+        <button onClick={openModal}>新規作成</button>
+        <Modal
+          showFlag={showModal}
+          setShowModal={setShowModal}
+          cancelHandler={HandleCancel}
+          okHandler={HandleOk}
+        >
+          <p>親から渡された値です。</p>
+        </Modal>
+      </div>
+      <ul>
+        {todoItems.map((todoItem)=> (<Lists key={todoItem.id} {...todoItem}/> ))}
+      </ul>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/Modal.tsx
@@ -0,0 +1,33 @@
+import './modal.css'
+interface modalProps{
+  showFlag: boolean;
+  children: React.ReactNode;
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
+  okHandler: () => void;
+  cancelHandler: () => void;
+}
+
+
+function Modal({
+    showFlag,
+    cancelHandler,
+    okHandler,
+    children
+  }:modalProps){
+  return(
+    showFlag?(
+      <div id="overlay" className="overlay" >
+        <div id="modalContent" className="modalContent">
+          <div>
+            <button id="topBotton" className='topButton' onClick={cancelHandler}>cancel</button>
+            <h1>modal</h1>
+            <div>{children}</div>
+            <button onClick={okHandler}>OK</button>
+          </div>
+        </div>
+      </div>
+    ):
+      null 
+  )
+} 
+export default Modal;
