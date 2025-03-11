df631e4f5699d684f7a147275646cb8a130a69d9
hkhb <h14hbk@gmail.com>
2025-03-11 16:10:24 +0900

    vite.config.tsの修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from "node:path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	base: "./",
+	root: "src",
+	plugins: [react()],
+	publicDir: resolve(__dirname, "public"),
+	build: {
+		// distフォルダに出力
+		outDir: resolve(__dirname, "dist"),
+		// 存在しないときはフォルダを作成する
+		emptyOutDir: true,
+		copyPublicDir: true,
+		rollupOptions: {
+			// entry pointがあるindex.htmlのパス
+			input: {
+				"": resolve(__dirname, "src/index.html"),
+			},
+			// bundle.jsを差し替えする
+			output: {
+				entryFileNames: "assets/bundle.js",
+			},
+		},
+	},
+});
