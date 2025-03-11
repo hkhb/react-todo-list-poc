1963c54dd567861b0b144fd15e7d680e240c6ba6
hkhb <h14hbk@gmail.com>
2025-03-11 16:12:43 +0900

    コメント削除

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { resolve } from "node:path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	base: "./",
 	root: "src",
