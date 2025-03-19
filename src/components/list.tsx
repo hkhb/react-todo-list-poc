5d7c3ae4fef087a0ff64e4ff0659b802b390d21d
hkhb <h14hbk@gmail.com>
2025-03-19 23:55:51 +0900

    編集機能を実装

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { format } from 'date-fns';
 import "./list.css"
 import { TodoItem } from '../App.tsx'
-// import Modal from './Modal.tsx';
 
 interface TodoItemWithClick extends TodoItem {
   onClickEdit: () => void;
