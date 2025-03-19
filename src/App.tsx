0873390c8f2c340ca0f8d522ce1c6662a0507b65
hkhb <h14hbk@gmail.com>
2025-03-20 00:44:56 +0900

    編集をキャンセルすると前回のtitleなどがままのバグを解消

--- src/App.tsx
+++ src/App.tsx
@@ -147,8 +147,8 @@ function App() {
     setTodoItems((prevItems) =>
       prevItems.map((item) =>
         item.id === id ? {...item, title: title, description: description, updatedAt: new Date() } : item
-      ));
-      closeModal();
+    ));
+    closeModal();
   }
 
   return (
