2fc97e905f2dde38bb2483a54675259976436c47
hkhb <h14hbk@gmail.com>
2025-03-19 23:55:51 +0900

    編集機能を実装

--- src/App.tsx
+++ src/App.tsx
@@ -211,14 +211,14 @@ function App() {
   //   if(!title){
   //     alert("titleを入力してください")
   //     return;
-  //   }
+  //   }7 (編集機能を実装)
     
-  //   setTodoItems((prevItems) =>
-  //     prevItems.map((item) =>
-  //       item.id === id ? {...item, title: title, description: description, updatedAt: new Date() } : item
-  //     ));
-  //     closeModal();
-  // }
+    setTodoItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? {...item, title: title, description: description, updatedAt: new Date() } : item
+      ));
+      closeModal();
+  }
 
   useEffect(() => {
     setNewTitle(ListTitle);
