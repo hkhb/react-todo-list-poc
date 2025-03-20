fe218b09e004391c122a67618112934c08acd906
hkhb <h14hbk@gmail.com>
2025-03-20 11:58:10 +0900

    型が見つからないエラーを修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,25 @@
-import { resolve } from "node:path";
+import path from "node:path";
 import { fileURLToPath } from "node:url";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
 	base: "./",
 	plugins: [react()],
-	publicDir: resolve(__dirname, "public"),
+	publicDir: path.resolve(__dirname, "public"),
 	build: {
 		// distフォルダに出力
-		outDir: resolve(__dirname, "dist"),
+		outDir: path.resolve(__dirname, "dist"),
 		// 存在しないときはフォルダを作成する
 		emptyOutDir: true,
 		copyPublicDir: true,
 		rollupOptions: {
 			// entry pointがあるindex.htmlのパス
 			input: {
-				"": resolve(__dirname, "index.html"),
+				"": path.resolve(__dirname, "index.html"),
 			},
 			// bundle.jsを差し替えする
 			output: {
