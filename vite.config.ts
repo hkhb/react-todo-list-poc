c55af52986f1b923e57344b1006acd5da42dd922
hkhb <h14hbk@gmail.com>
2025-03-20 11:51:03 +0900

    tsconfig.jsonを修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
 	base: "./",
