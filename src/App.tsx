b35a466c796397e0c6df041c878b128fc5fa411b
hkhb <h14hbk@gmail.com>
2025-03-22 10:59:39 +0900

    削除関数の実装

--- src/App.tsx
+++ src/App.tsx
@@ -136,6 +136,7 @@ function App() {
   //titleがある場合
   //戻り値　なし
   //titleがない場合
+  //alartを出す
   //戻り値　なし
   //titleがない場合は、alartを出す
   const onEditList = (title: string, description: string, id: number) => {
@@ -177,6 +178,31 @@ function App() {
     } else {
       onAddList(newTitle, newDescription);
     }
+  }
+  //リストの削除
+  //引数 id
+  //idがある場合 
+  //alartを出す(削除OK？)
+  //戻り値なし
+  //idがない場合 
+  //alartを出す(idがない)
+  //戻り値なし 
+  //現在のtodolistを新しい変数で受け取る
+  //リストごとに分割
+  //idが同じでない場合、newtodolistに格納 
+  //idが同じ場合、何もしない
+  //setterでnewtodolistに変更
+  const onDelete = (id:number) =>{
+    if(!id){
+      alert("idがありません")
+      return;
+    }
+    const prevtodoItems:TodoItem[] = todoItems
+    const newtodoItems:TodoItem[] = 
+    prevtodoItems.filter((Item:TodoItem) => Item.id !== id
+    )
+    setTodoItems(newtodoItems);
+    alert("削除しますか？")
   }
 
   useEffect(() => {
@@ -226,8 +252,9 @@ function App() {
           <Lists
             key={todoItem.id}
             {...todoItem}
-            onClick={() => onClickList(todoItem)}
-          />
+            onClickEdit={() => onListClick(todoItem)}
+            onClickDelete={() => onDelete(todoItem.id)}
+          /> 
         ))}
       </ul>
     </div>
