cca614aa4681eff1e8b497af4cb942558c7080bd
hkhb <h14hbk@gmail.com>
2025-04-13 07:41:52 +0900

    移動

--- src/App.tsx
+++ src/App.tsx
@@ -136,8 +136,8 @@ function App() {
     closeModal();
   }
   
-  const [newTitle, setNewTitle] = useState("");
-  const [newDescription, setNewDescription] = useState("");
+  // const [newTitle, setNewTitle] = useState("");
+  // const [newDescription, setNewDescription] = useState("");
   const ListTitle:string = ((isEdit && editList) ? editList.title : "");
   const ListDescription:string = ((isEdit && editList)?editList.description ?? "" :"");
 
@@ -147,18 +147,18 @@ function App() {
   // isEditとeditListがあればonEditListを実行
   // なければonAddListを実行
 
-  function onOk(){
-    if(isEdit&&editList){
-      onEditList(newTitle, newDescription, editList.id);
-    }else{
-      onAddList(newTitle, newDescription);
-    }
-  };
+  // function onOk(){
+  //   if(isEdit&&editList){
+  //     onEditList(newTitle, newDescription, editList.id);
+  //   }else{
+  //     onAddList(newTitle, newDescription);
+  //   }
+  // };
 
-  useEffect(() => {
-    setNewTitle(ListTitle);
-    setNewDescription(ListDescription);
-  }, [ListTitle, ListDescription, showModal]);
+  // useEffect(() => {
+  //   setNewTitle(ListTitle);
+  //   setNewDescription(ListDescription);
+  // }, [ListTitle, ListDescription, showModal]);
 
   const list = editList? editList : undefined;
 
