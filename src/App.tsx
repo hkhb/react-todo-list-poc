8e64e3179bf6218e62478e9436a571bde02e7583
hkhb <h14hbk@gmail.com>
2025-04-14 21:59:08 +0900

    初期値がない場合を追加

--- src/App.tsx
+++ src/App.tsx
@@ -13,18 +13,18 @@ export interface TodoItem {
   updatedAt?: Date;   // 更新日時（任意）
 }
 
-const initialTodoItems:TodoItem[] = [
-  {id : 1, title: "掃除", description: "キッチンの掃除", completed: false, createdAt: new Date(2025, 3, 4), updatedAt: new Date(2025, 3, 4)},
-  {id : 2, title: "洗濯", completed: true, createdAt: new Date(20250304)},
-  {id : 3, title: "料理", description: "味噌汁", completed: false, createdAt: new Date(2025, 3, 4)},
-  {id : 4, title: "掃除", description: "1234", completed: false, createdAt: new Date()},
-  {id : 5, title: "掃除", completed: false, createdAt: new Date(), updatedAt: new Date()},
-  {id : 6, title: "掃除", description: "true", completed: true, createdAt: new Date(2025, 3, 4)},
-  {id : 7, title: "1223", description: "null", completed: false, createdAt: new Date(2025, 3, 4)},
-  {id : 8, title: "", description: "", completed: false, createdAt: new Date(20250304)},
-  {id : 9, title: "掃除", description: "キッチンの掃除", completed: true, createdAt: new Date(2025, 3, 4)},
-  {id : 10, title: "掃除", description: "キッチンの掃除", completed: false, createdAt: new Date(20250304)},
-]
+// const initialTodoItems:TodoItem[] = [
+//   {id : 1, title: "掃除", description: "キッチンの掃除", completed: false, createdAt: new Date(2025, 3, 4), updatedAt: new Date(2025, 3, 4)},
+//   {id : 2, title: "洗濯", completed: true, createdAt: new Date(20250304)},
+//   {id : 3, title: "料理", description: "味噌汁", completed: false, createdAt: new Date(2025, 3, 4)},
+//   {id : 4, title: "掃除", description: "1234", completed: false, createdAt: new Date()},
+//   {id : 5, title: "掃除", completed: false, createdAt: new Date(), updatedAt: new Date()},
+//   {id : 6, title: "掃除", description: "true", completed: true, createdAt: new Date(2025, 3, 4)},
+//   {id : 7, title: "1223", description: "null", completed: false, createdAt: new Date(2025, 3, 4)},
+//   {id : 8, title: "", description: "", completed: false, createdAt: new Date(20250304)},
+//   {id : 9, title: "掃除", description: "キッチンの掃除", completed: true, createdAt: new Date(2025, 3, 4)},
+//   {id : 10, title: "掃除", description: "キッチンの掃除", completed: false, createdAt: new Date(20250304)},
+// ]
 
 function App() {
 
@@ -65,14 +65,26 @@ function App() {
   //戻り値　なし
   const onAddList = (title:string, description:string) => {
     if(!!title){
-      const newTodo:TodoItem = {
-        id: todoItems.length + 1,
-        title,
-        description,
-        completed: false,
-        createdAt: new Date(),
+      if(todoItems){
+        const newTodo:TodoItem = {
+          id: todoItems.length + 1,
+          title,
+          description,
+          completed: false,
+          createdAt: new Date(),
+        }
+        setTodoItems([...todoItems, newTodo]);
+      }else{
+        const newTodo:TodoItem = {
+          id: 1,
+          title,
+          description,
+          completed: false,
+          createdAt: new Date(),
+        }
+        setTodoItems([newTodo]);
       }
-      setTodoItems([...todoItems, newTodo]);
+      localStorage.setItem('todoItems', JSON.stringify(todoItems));
       closeModal();
     }else{
       alert("titleを入力してください")
@@ -95,9 +107,10 @@ function App() {
       prevItems.map((item) =>
         item.id === id ? {...item, title: title, description: description, updatedAt: new Date() } : item
     ));
+    localStorage.setItem('todoItems', JSON.stringify(todoItems));
     closeModal();
   }
-  const list = editList? editList : null;
+  const list = editList? editList : undefined;
 
   return (
     <div className='container'>
