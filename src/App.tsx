b60cde9982444bba565435e23a8cc1305620c95f
hkhb <h14hbk@gmail.com>
2025-04-16 12:38:26 +0900

    インデントの修正

--- src/App.tsx
+++ src/App.tsx
@@ -133,12 +133,12 @@ function App() {
     }
     const confirm = window.confirm("本当に削除しますか？");
     if(confirm){
-    const prevtodoItems:TodoItem[] = todoItems
-    const newtodoItems:TodoItem[] = 
-    prevtodoItems.filter((Item:TodoItem) => Item.id !== id
-    )
-    setTodoItems(newtodoItems);
-  }
+      const prevtodoItems:TodoItem[] = todoItems
+      const newtodoItems:TodoItem[] = 
+        prevtodoItems.filter((Item:TodoItem) => Item.id !== id
+        )
+      setTodoItems(newtodoItems);
+    }   
   }
   const onAddList = (title:string, description:string) => {
     if(!!title){
