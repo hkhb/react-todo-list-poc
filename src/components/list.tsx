84ce818a2396b06e1738448f3d16fa52f5e22ade
hkhb <h14hbk@gmail.com>
2025-04-22 23:38:18 +0900

    filename

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { format } from 'date-fns';
-import "./List.css"
+import "./list.css"
 import { TodoItem } from '../App.tsx'
 
 interface TodoItemWithClick extends TodoItem {
