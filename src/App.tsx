e8cb8ae798f8ae08895e1184a32244285c96d9e9
hkhb <h14hbk@gmail.com>
2025-04-22 23:25:20 +0900

    不要なものを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect,} from 'react';
+import { useState, useEffect} from 'react';
 import "./App.css"
 import Modal from './components/Modal.tsx'
 import Lists from "./components/list.tsx"
